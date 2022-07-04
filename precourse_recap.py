ffe9859c4ed754f3dbdeacfe68a1e71a33ab60f7
Alejandro Garcia <alejandrojg8@gmail.com>
2022-07-04 20:24:52 +0100

    added new variable

--- precourse_recap.py
+++ precourse_recap.py
@@ -7,8 +7,9 @@ country_2 = "France"
 total_1 = num_1 + num_2
 total_2 = num_1 - num_3
 total_3 = num_2 * num_1
+fav_countries = country_1 + " and " + country_2
 
 print(total_1)
 print(total_2)
 print(total_3)
-country_1 + " " + country_2
+print(fav_countries)
