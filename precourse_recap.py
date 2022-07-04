7337a61c4e9eeaa747595bdd333950126318a6e8
Alejandro Garcia <alejandrojg8@gmail.com>
2022-07-04 20:09:01 +0100

    added concatenation of strings

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,13 +1,14 @@
 num1 = 8
 num2 = 3
 num3 = 6.7
-country = "Spain"
-country = "France"
+country1 = "Spain"
+country2 = "France"
 
- total1 = num1 + num2
+total1 = num1 + num2
 total2 = num1 - num3
 total3 = num2 * num1
 
 print(total1)
 print(total2)
 print(total3)
+country1 + " " + country2
